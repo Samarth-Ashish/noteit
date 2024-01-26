949bdf36cbbf9ba887d0f400dc49971af1e48bcb
Ashish <samarthashish111@gmail.com>
2024-01-27 03:33:28 +0530

    ..

--- lib/create_note.dart
+++ lib/create_note.dart
@@ -1,6 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:popover/popover.dart';
+// import 'package:popover/popover.dart';
+// import 'package:flex_color_scheme/flex_color_scheme.dart';
+import 'main.dart';
+import 'package:provider/provider.dart';
+
+Color? darken(Color? color, [double amount = .1]) {
+  if (color == null) {
+    return null;
+  }
+
+  assert(amount >= 0 && amount <= 1);
+
+  final hsl = HSLColor.fromColor(color);
+  final hslDark = hsl.withLightness((hsl.lightness - amount).clamp(0.0, 1.0));
+
+  return hslDark.toColor();
+}
+
+Color? lighten(Color? color, [double amount = .1]) {
+  if (color == null) {
+    return null;
+  }
+
+  assert(amount >= 0 && amount <= 1);
+
+  final hsl = HSLColor.fromColor(color);
+  final hslLight = hsl.withLightness((hsl.lightness + amount).clamp(0.0, 1.0));
+
+  return hslLight.toColor();
+}
 
 class CreateNotePage extends StatefulWidget {
   const CreateNotePage({super.key});
@@ -12,17 +41,27 @@ class CreateNotePage extends StatefulWidget {
 class _CreateNotePageState extends State<CreateNotePage> {
   String currentTitle = '';
   TextEditingController titleController = TextEditingController();
-  // bool isTitleTextSubmitted = false;
   FocusNode focusNode = FocusNode();
   bool isColorPickerActive = false;
+  Color? currentColor;
+  List<Color> colorList = [
+    Colors.transparent,
+    Colors.green,
+    Colors.blue,
+    Colors.yellow,
+    Colors.red,
+    Colors.pink,
+    // Colors.orange
+  ];
 
-  Container textBox() {
+  Container titleTextField() {
     return Container(
       alignment: Alignment.center,
       // color: Colors.white,
       child: TextField(
         style: const TextStyle(
           fontSize: 20,
+          fontWeight: FontWeight.bold,
         ),
         textAlign: TextAlign.center,
         focusNode: focusNode,
@@ -34,13 +73,16 @@ class _CreateNotePageState extends State<CreateNotePage> {
           hintText: 'Title',
           hintStyle: const TextStyle(
             fontSize: 20,
+            // color: darken(currentColor, .9),
+            // decorationColor: Colors.yellow,
+            // backgroundColor: Colors.white,
           ),
         ),
         // autofocus: true,
         onChanged: (text) => {
           setState(() {
             currentTitle = titleController.text;
-            print(text);
+            debugPrint(text);
           })
         },
       ),
@@ -51,7 +93,7 @@ class _CreateNotePageState extends State<CreateNotePage> {
   void initState() {
     super.initState();
     focusNode.addListener(() {
-      print(focusNode.hasFocus ? 'in' : 'out');
+      debugPrint(focusNode.hasFocus ? 'in' : 'out');
       setState(() {});
     });
   }
@@ -65,213 +107,221 @@ class _CreateNotePageState extends State<CreateNotePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        automaticallyImplyLeading: false,
-        title: textBox(),
-        // leading: IconButton(
-        //   onPressed: () {
-        //     showPopover(
-        //       context: context,
-        //       bodyBuilder: (context) => Padding(
-        //         padding: const EdgeInsets.symmetric(vertical: 8),
-        //         child: ListView(
-        //           padding: const EdgeInsets.all(8),
-        //           children: [
-        //             InkWell(
-        //               // onTap: () {
-        //               //   Navigator.of(context)
-        //               //     ..pop()
-        //               //     ..push(
-        //               //       MaterialPageRoute<SecondRoute>(
-        //               //         builder: (context) => SecondRoute(),
-        //               //       ),
-        //               //     );
-        //               // },
-        //               child: Container(
-        //                 height: 50,
-        //                 color: Colors.amber[100],
-        //                 child: const Center(child: Text('Entry A')),
-        //               ),
-        //             ),
-        //             const Divider(),
-        //             Container(
-        //               height: 50,
-        //               color: Colors.amber[200],
-        //               child: const Center(child: Text('Entry B')),
-        //             ),
-        //             const Divider(),
-        //             Container(
-        //               height: 50,
-        //               color: Colors.amber[300],
-        //               child: const Center(child: Text('Entry C')),
-        //             ),
-        //           ],
-        //         ),
-        //       ),
-        //       onPop: () => print('Popover was popped!'),
-        //       direction: PopoverDirection.bottom,
-        //       width: 200,
-        //       height: 400,
-        //       arrowHeight: 15,
-        //       arrowWidth: 30,
-        //     );
-        //   },
-        //   icon: const Icon(
-        //     Icons.palette,
-        //   ),
-        // ),
-        leading: PopupMenuButton(
-          // onSelected: (value) {
-          //   setState(() {
-          //     print('tapped');
-          //   });
-          // },
-          onOpened: () {
-            setState(() {
-              isColorPickerActive = true;
-            });
-          },
-          onCanceled: () {
-            setState(() {
-              isColorPickerActive = false;
-            });
-          },
-          onSelected: (value) {
-            setState(() {
-              isColorPickerActive = false;
-            });
-          },
-          icon: Icon(
-            isColorPickerActive ? Icons.palette_outlined : Icons.palette,
-          ),
-          // elevation: 10,
-          itemBuilder: (context) => [
-            PopupMenuItem(
-              value: 1,
-              child: Row(
-                children: [
-                  IconButton(
-                    onPressed: () {},
-                    icon: const Icon(
-                      Icons.circle,
-                      color: Colors.green,
-                    ),
-                  ),
-                  IconButton(
-                    onPressed: () {},
-                    icon: const Icon(
-                      Icons.circle,
-                      color: Colors.blue,
-                    ),
-                  ),
-                  IconButton(
-                    onPressed: () {},
-                    icon: const Icon(
-                      Icons.circle,
-                      color: Colors.yellow,
-                    ),
-                  ),
-                  IconButton(
-                    onPressed: () {},
-                    icon: const Icon(
-                      Icons.circle,
-                      color: Colors.red,
-                    ),
-                  ),
-                  IconButton(
-                    onPressed: () {},
-                    icon: const Icon(
-                      Icons.circle,
-                      color: Colors.pink,
-                    ),
-                  )
-                ],
+    return Theme(
+      data: (currentColor != null)
+          ? ThemeData(
+              //!Usual
+              // primarySwatch: MaterialColor(1, ),
+              colorScheme: ColorScheme.fromSeed(
+                seedColor: currentColor!,
+                background: currentColor!,
+                brightness: Provider.of<ThemeProvider>(context).isDark
+                    ? Brightness.dark
+                    : Brightness.light,
               ),
-            ),
-          ],
-          offset: const Offset(0, 60),
-          // color: Colors.grey,
-          // elevation: 2,
-        ),
-        actions: [
-          // IconButton(
-          //   icon: const Icon(Icons.more_vert),
-          //   onPressed: () {},
-          // )
-          PopupMenuButton(
-            itemBuilder: (context) => [
-              // popupmenu item 1
-              const PopupMenuItem(
-                value: 1,
-                // row has two child icon and text.
-                child: Row(
-                  children: [
-                    Icon(Icons.star),
-                    SizedBox(
-                      // sized box with width 10
-                      width: 10,
-                    ),
-                    Text("Get The App")
-                  ],
+              appBarTheme: AppBarTheme(
+                backgroundColor: currentColor!,
+                actionsIconTheme: IconThemeData(
+                  color: darken(currentColor, .5),
+                ),
+                iconTheme: IconThemeData(
+                  color: darken(currentColor, .5),
                 ),
               ),
-              // popupmenu item 2
-              const PopupMenuItem(
-                value: 2,
-                // row has two child icon and text
+            )
+          : Provider.of<ThemeProvider>(context).currentTheme,
+      // // ),
+      // data: FlexThemeData.dark(// !FlexThemeData
+      //     // scheme: FlexScheme.mallardGreen,
+      //     // colors: FlexColorScheme.createPrimarySwatch(Colors.blue),
+      //     // blendLevel: 1,
+      //     ),
+      //
+      child: Scaffold(
+        // backgroundColor: currentColor,
+        // extendBody: true,
+        appBar: AppBar(
+          centerTitle: true,
+          automaticallyImplyLeading: false,
+          title: titleTextField(),
+          // leading: IconButton(
+          //   onPressed: () {
+          //     showPopover(//!showPopover
+          //       context: context,
+          //       bodyBuilder: (context) => Padding(
+          //         padding: const EdgeInsets.symmetric(vertical: 8),
+          //         child: ListView(
+          //           padding: const EdgeInsets.all(8),
+          //           children: [
+          //             InkWell(
+          //               // onTap: () {
+          //               //   Navigator.of(context)
+          //               //     ..pop()
+          //               //     ..push(
+          //               //       MaterialPageRoute<SecondRoute>(
+          //               //         builder: (context) => SecondRoute(),
+          //               //       ),
+          //               //     );
+          //               // },
+          //               child: Container(
+          //                 height: 50,
+          //                 color: Colors.amber[100],
+          //                 child: const Center(child: Text('Entry A')),
+          //               ),
+          //             ),
+          //             const Divider(),
+          //             Container(
+          //               height: 50,
+          //               color: Colors.amber[200],
+          //               child: const Center(child: Text('Entry B')),
+          //             ),
+          //             const Divider(),
+          //             Container(
+          //               height: 50,
+          //               color: Colors.amber[300],
+          //               child: const Center(child: Text('Entry C')),
+          //             ),
+          //           ],
+          //         ),
+          //       ),
+          //       onPop: () => print('Popover was popped!'),
+          //       direction: PopoverDirection.bottom,
+          //       width: 200,
+          //       height: 400,
+          //       arrowHeight: 15,
+          //       arrowWidth: 30,
+          //     );
+          //   },
+          //   icon: const Icon(
+          //     Icons.palette,
+          //   ),
+          // ),
+          leading: PopupMenuButton(
+            //!PopupMenuButton
+            onOpened: () {
+              setState(() {
+                isColorPickerActive = true;
+              });
+            },
+            onCanceled: () {
+              setState(() {
+                isColorPickerActive = false;
+              });
+            },
+            onSelected: (value) {
+              setState(() {
+                isColorPickerActive = false;
+                debugPrint('tapped');
+              });
+            },
+            icon: Icon(
+              isColorPickerActive ? Icons.palette_outlined : Icons.palette,
+            ),
+            // elevation: 10,
+            itemBuilder: (context) => [
+              PopupMenuItem(
+                // value: 1,
                 child: Row(
-                  children: [
-                    Icon(Icons.chrome_reader_mode),
-                    SizedBox(
-                      // sized box with width 10
-                      width: 10,
+                  children: List.generate(
+                    colorList.length,
+                    (index) => IconButton(
+                      onPressed: () {
+                        setState(() {
+                          print(colorList[index]);
+                          currentColor = colorList[index] == Colors.transparent
+                              ? null
+                              : colorList[index].withOpacity(0.5);
+                        });
+                      },
+                      icon: colorList[index] != Colors.transparent
+                          ? Icon(
+                              Icons.circle,
+                              color: colorList[index],
+                            )
+                          : Icon(
+                              Icons.water_drop_rounded,
+                              color: Theme.of(context).highlightColor,
+                            ),
                     ),
-                    Text("About")
-                  ],
+                  ),
                 ),
               ),
             ],
-            // offset: const Offset(0, 100),
+            offset: const Offset(0, 60),
             // color: Colors.grey,
             // elevation: 2,
           ),
-        ],
-      ),
-      body: const Padding(
-        padding: EdgeInsets.all(8.0),
-        child: Column(
-          children: [
-            // Row(
-            //   mainAxisAlignment: MainAxisAlignment.center,
-            //   children: [
-            //     IconButton(
-            //         onPressed: () {
-            //           setState(() {});
-            //         },
-            //         icon: const Icon(
-            //           Icons.palette,
-            //         ))
-            //   ],
-            // ),
-
-            TextField(
-              keyboardType: TextInputType.multiline,
-              maxLines: null,
-              decoration: InputDecoration(
-                border: InputBorder.none,
-                hintText: 'Note',
-              ),
+          actions: [
+            // IconButton(
+            //   icon: const Icon(Icons.more_vert),
+            //   onPressed: () {},
+            // )
+            PopupMenuButton(
+              itemBuilder: (context) => [
+                // popupmenu item 1
+                const PopupMenuItem(
+                  value: 1,
+                  // row has two child icon and text.
+                  child: Row(
+                    children: [
+                      Icon(Icons.star),
+                      SizedBox(
+                        // sized box with width 10
+                        width: 10,
+                      ),
+                      Text("Get The App")
+                    ],
+                  ),
+                ),
+                // popupmenu item 2
+                const PopupMenuItem(
+                  value: 2,
+                  // row has two child icon and text
+                  child: Row(
+                    children: [
+                      Icon(Icons.chrome_reader_mode),
+                      SizedBox(
+                        // sized box with width 10
+                        width: 10,
+                      ),
+                      Text("About")
+                    ],
+                  ),
+                ),
+              ],
+              // offset: const Offset(0, 100),
+              // color: Colors.grey,
+              // elevation: 2,
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.of(context).pop();
-        },
-        child: const Icon(Icons.check),
+        body: const SizedBox(
+          width: double.infinity,
+          height: double.infinity,
+          // color: Colors.amber,
+          child: Padding(
+            padding: EdgeInsets.all(10),
+            child: Column(
+              children: [
+                TextField(
+                  keyboardType: TextInputType.multiline,
+                  maxLines: null,
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: 'Note',
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          // backgroundColor: currentColor,
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          child: const Icon(Icons.check),
+        ),
       ),
     );
   }
